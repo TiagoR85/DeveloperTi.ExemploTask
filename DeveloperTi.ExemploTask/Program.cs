06ccc089880c903581fc9964249c82f7001e84dc
Tiago Rodrigues <tiago.rds85@hotmail.com>
2020-09-02 18:00:43 -0300

    Adicionar arquivos de projeto.

---
+++ DeveloperTi.ExemploTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeveloperTi.ExemploTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
