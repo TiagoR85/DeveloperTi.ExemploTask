04d221fd67450e28a884d87250d384bc7e01e7bb
Tiago Rodrigues <tiago.rds85@hotmail.com>
2020-09-03 17:41:39 -0300

    Dividido em dois aplicativos de console.

--- DeveloperTi.ExemploTask/Program.cs
+++ DeveloperTi.ExemploTask/Program.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace DeveloperTi.ExemploTask
+namespace DeveloperTi.ExemploParalellInvoke
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // Retrieve Goncharov's "Oblomov" from Gutenberg.org.
+            string[] words = CreateWordArray(@"http://www.gutenberg.org/files/54700/54700-0.txt");
+
+            #region ParallelTasks
+            // Perform three tasks in parallel on the source array
+            Parallel.Invoke(() =>
+            {
+                Console.WriteLine("Begin first task...");
+                GetLongestWord(words);
+            },  // close first Action
+            () =>
+            {
+                Console.WriteLine("Begin second task...");
+                GetMostCommonWords(words);
+            }, //close second Action
+            () =>
+            {
+                Console.WriteLine("Begin third task...");
+                GetCountForWord(words, "sleep");
+            } //close third Action
+            ); //close parallel.invoke
+
+            Console.WriteLine("Returned from Parallel.Invoke");
+            #endregion
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
+
+        #region HelperMethods
+        private static void GetCountForWord(string[] words, string term)
+        {
+            var findWord = words.Where(w => w.ToUpper().Contains(term.ToUpper())).Select(w => w);
+            Console.WriteLine($@"Task 3 -- The word ""{term}"" occurs {findWord.Count()} times.");
+        }
+
+        private static void GetMostCommonWords(string[] words)
+        {
+            var frequencyOrder = words.Where(w => w.Length > 6)
+                                      .GroupBy(w => w)
+                                      .OrderByDescending(g => g.Count())
+                                      .Select(r => r.Key);
+
+            var commonWords = frequencyOrder.Take(10);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Task 2 -- The most common words are:");
+            foreach (var v in commonWords)
+            {
+                sb.AppendLine("  " + v);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        private static string GetLongestWord(string[] words)
+        {
+            var longestWord = words.OrderByDescending(w => w.Length).First();
+            Console.WriteLine($"Task 1 -- The longest word is {longestWord}.");
+            return longestWord;
+        }
+
+        // An http request performed synchronously for simplicity.
+        static string[] CreateWordArray(string uri)
+        {
+            Console.WriteLine($"Retrieving from {uri}");
+
+            // Download a web page the easy way.
+            string s = new WebClient().DownloadString(uri);
+
+            // Separate string into an array of words, removing some common punctuation.
+            return s.Split(new char[] { ' ', '\u000A', ',', '.', ';', ':', '-', '_', '/' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+        #endregion
     }
 }
---
+++ DeveloperTi.Task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeveloperTi.ExemploTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+            Task taskA = Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Hello from taskA.");
+            });
+            Console.WriteLine("Hello from thread '{0}'.", Thread.CurrentThread.Name);
+            Thread.Sleep(4000);
+            taskA.Wait();//aguardável da thread.
+        }
+    }
+}
